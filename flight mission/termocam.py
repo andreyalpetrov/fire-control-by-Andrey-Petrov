4b1c2addfb7f976e3ec737ae542a420cb3d7f5dd
andreyalpetrov <128998530+andreyalpetrov@users.noreply.github.com>
2026-02-01 17:37:31 +0300

    Update termocam.py

--- flight mission/termocam.py
+++ flight mission/termocam.py
@@ -1,22 +1,39 @@
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, Temperature
 from cv_bridge import CvBridge
-import cv2
+import numpy as np
+
+from p2pro_wrapper import P2Pro   # файл, где лежит класс выше
 
 rospy.init_node('termocam_publisher')
-pub = rospy.Publisher('termocam', Image, queue_size=10)
+pub_image = rospy.Publisher('/termocam_image', Image, queue_size=10)
+pub_temp  = rospy.Publisher('/termocam_temp', Temperature, queue_size=10)
 bridge = CvBridge()
-cap = cv2.VideoCapture(1)
+
+cam_id = '/dev/video1'  # лучше явно путь
+cam = P2Pro(cam_id)
 
 rate = rospy.Rate(25)
 while not rospy.is_shutdown():
-    ret, frame = cap.read()
-    if not ret:
+    temp_celsius = cam.temperature()
+    if temp_celsius is None:
         rospy.logwarn("Нет кадра с камеры")
         continue
 
-    image_msg = bridge.cv2_to_imgmsg(frame, encoding="bgr8")
-    pub.publish(image_msg)
+    print(f"min={temp_celsius.min():.1f}°C  max={temp_celsius.max():.1f}°C")
+
+    # нормализация в 0..255 и uint8
+    display = (temp_celsius - temp_celsius.min()) / (temp_celsius.max() - temp_celsius.min())
+    display_u8 = (display * 255).astype(np.uint8)
+
+    # 1‑канальное изображение
+    image_msg = bridge.cv2_to_imgmsg(display_u8, encoding="mono8")
+    pub_image.publish(image_msg)
     rate.sleep()
 
-cap.release()
+    temp_max_msg = Temperature()
+    temp_max_msg.header.stamp = rospy.Time.now()
+    temp_max_msg.header.frame_id = "/termocam"
+    temp_max_msg.temperature = temp_celsius.max()
+    temp_max_msg.variance = 0.0
+    pub_temp.publish(temp_max_msg)
